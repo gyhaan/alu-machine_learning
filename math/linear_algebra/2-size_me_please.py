d26830089371da697701eca11b62580450b25056
gyhaan <gyhaan@alustudent.com>
2025-01-24 05:10:10 -0800

    try again

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,18 +1,28 @@
-#!/usr/bin/env python3
-
 def matrix_shape(arr):
-    arr1 = [len(arr)]
-    def check_arr_size(arr2):
-        if(isinstance(arr2, list)):
-            arr1.append(len(arr2))
-            check_arr_size(arr2[0])
-    
-    check_arr_size(arr[0])
-    return arr1
+    """
+    Calculates the shape of a matrix (nested list) as a list of integers.
 
+    Args:
+        arr (list): A nested list representing the matrix.
+
+    Returns:
+        list: The shape of the matrix as a list of integers.
+    """
+    shape = []
+
+    def check_arr_size(current_arr):
+        if isinstance(current_arr, list):
+            shape.append(len(current_arr))
+            check_arr_size(current_arr[0])
+
+    check_arr_size(arr)
+    return shape
+
+
+# Examples
 mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
+print(matrix_shape(mat1))  # Output: [2, 2]
 
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))
+mat2 = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
+print(matrix_shape(mat2))  # Output: [2, 2, 3]
+
